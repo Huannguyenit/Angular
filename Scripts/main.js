9dd8c5ff7e7f8f749fc884a771e731e7047a9d0d
Huan Nguyen <huannguyen13cntt@gmail.com>
2017-01-10 22:42:39 +0700

    Modified index.html add condition for show up gem and button, modified main.js now gems is an array - add 2 boolean variable soldOut and canPurcharse

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,11 +1,22 @@
 (function(){
     var app = angular.module("Main",[]);
     app.controller("MainController",function(){
-        this.product = gem
+        this.product = gems
     });
-    var gem = {
+    var gems = [
+        {
+        soldOut: true,
         name: "Ruby",
         price: 3000000,
-        description: "....."
-    }
+        description: ".....",
+        canPurcharse: true
+        },
+        {
+            soldOut: false,
+            name: "Pearl",
+            price: 3000000,
+            description: "Not cheaper",
+            canPurcharse: true
+        }
+    ]
 })();
