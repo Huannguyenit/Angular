582e2b7f2088282e9f814d3c5ab250cea28d51ce
Huan Nguyen <huannguyen13cntt@gmail.com>
2017-01-10 22:11:48 +0700

    Added folder Scripts, added main.js, added index.html

---
+++ Scripts/main.js
@@ -0,0 +1,11 @@
+(function(){
+    var app = angular.module("Main",[]);
+    app.controller("MainController",function(){
+        this.product = gem
+    });
+    var gem = {
+        name: "Ruby",
+        price: 3000000,
+        description: "....."
+    }
+})();
